e9b00d679cdf4e2b94f8da5565097b2d63adf064
Obafemi Oludahunsi <oludahunsiobafemi18@gmail.com>
2022-08-18 00:32:00 +0100

    first commit

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="http://fonts.cdnfonts.com/css/ambit" rel="stylesheet" />
+        <link href="http://fonts.cdnfonts.com/css/archivo" rel="stylesheet" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
